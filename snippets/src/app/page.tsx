c24c0a254372020516323df8bd3ad61f4d290d84
Matheus Ferreira <matheus.fer33@gmail.com>
2024-03-24 20:58:59 -0300

    snippets: added home page showing all created snippets

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <p>home</p>;
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map(snippet => (
+    <div className="border" key={snippet.id}>
+      <h3>{snippet.title}</h3>
+      <pre>{snippet.code}</pre>
+    </div>
+  ));
+
+  return <>{renderedSnippets}</>;
 }
--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,10 +1,11 @@
+import { redirect } from 'next/navigation';
+
 import { db } from '@/db';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
-import { redirect } from 'next/navigation';
 
 export default function SnippetCreatePage() {
   async function createSnippet(formData: FormData) {
