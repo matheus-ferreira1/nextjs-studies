996fd7d3c77a75b2cc7d0e8fc71411c48382902e
Matheus Ferreira <matheus.fer33@gmail.com>
2024-03-24 21:37:34 -0300

    snippets: styling of home page

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,14 +1,39 @@
+import { buttonVariants } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { db } from '@/db';
+import { Heading3 } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
-  const renderedSnippets = snippets.map(snippet => (
-    <div className="border" key={snippet.id}>
-      <h3>{snippet.title}</h3>
-      <pre>{snippet.code}</pre>
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-lg my-4">Snippets</h1>
+        <Link className={buttonVariants()} href="/snippets/new">
+          New Snippet
+        </Link>
+      </div>
+      <div className="space-y-2">
+        {snippets.length > 0 ? (
+          snippets.map(snippet => (
+            <Card key={snippet.id}>
+              <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle>{snippet.title}</CardTitle>
+                <Link
+                  className={buttonVariants({ variant: 'outline' })}
+                  href={`/snippets/${snippet.id}`}
+                >
+                  View
+                </Link>
+              </CardHeader>
+            </Card>
+          ))
+        ) : (
+          <h3>There are no snippets to show</h3>
+        )}
+      </div>
     </div>
-  ));
-
-  return <>{renderedSnippets}</>;
+  );
 }
